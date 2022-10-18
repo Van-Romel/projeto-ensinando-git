e291e79eb4dea8040b8c79888af6c6ab5c2ad69f
Van-Romel A S Neto <vanr.neto@hotmail.com>
2022-10-18 20:12:38 -0300

    initial commit

---
+++ src/com/romel/Pessoa.java
@@ -0,0 +1,22 @@
+package com.romel;
+
+public class Pessoa {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo");
+        if (args.length > 0)
+            System.out.println("Este é o primeiro argumento: " + args[0]);
+        else
+            System.out.println("Não foi recebido nenhum argumento.");
+        asçdkjfçlkajsdçf
+
+
+                açsjflkadasdasdasjsd~fk
+
+        if (args.length > 0)
+            System.out.println("Este é o primeiro argumento: " + args[0]);
+        else
+            System.out.println("Não foi recebido nenhum argumento.");
+
+
+    }
+}
