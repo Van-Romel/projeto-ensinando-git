c3d9a3d646d7b2fd1b92752ba7fc10e8bbfba218
Van-Romel A S Neto <vanr.neto@hotmail.com>
2022-10-18 20:53:19 -0300

    Removi a classe compilada, e altereia a classe Pessoa removendo codigo não funcional.

--- src/com/romel/Pessoa.java
+++ src/com/romel/Pessoa.java
@@ -7,16 +7,10 @@ public class Pessoa {
             System.out.println("Este é o primeiro argumento: " + args[0]);
         else
             System.out.println("Não foi recebido nenhum argumento.");
-        asçdkjfçlkajsdçf
-
-
-                açsjflkadasdasdasjsd~fk
 
         if (args.length > 0)
             System.out.println("Este é o primeiro argumento: " + args[0]);
         else
             System.out.println("Não foi recebido nenhum argumento.");
-
-
     }
 }
