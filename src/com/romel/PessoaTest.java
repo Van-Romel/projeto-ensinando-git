7400642d3d51ab0522211814df099557fe8f64d9
Van-Romel A S Neto <vanr.neto@hotmail.com>
2022-10-18 21:58:09 -0300

    Criei teste Unitario da classe Pessoa

---
+++ src/com/romel/PessoaTest.java
@@ -0,0 +1,18 @@
+package com.romel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PessoaTest {
+
+    public void quandoAbreOMapa_entaoOMapaECarregadoNaTela(){
+//        masçfçalskdç
+    }
+
+    public void quandoAbreOSemAcessoAInternet_entaoNaoChamaOMapa(){
+//        masçfçalskdç
+    }
+
+    public void testeNecessario(){
+
+    }
+}
